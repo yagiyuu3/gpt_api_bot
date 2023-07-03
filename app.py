1d1a6d4a7a26efe5408346b8d3bf9d72635bf7fa
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-03 12:04:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ user_input = st.text_input("メッセージを入力してください。", key=
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
         
-    for i message in enumerate(reversed(messages[1:])):  # 直近のメッセージを上に
+    for i, message in enumerate(reversed(messages[1:])):  # 直近のメッセージを上に
         speaker = "🙂"
         if message["role"]=="assistant":
             speaker="🤖"
