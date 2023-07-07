32e4798668dacc87cc057d72882f522f1271c73f
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-07 11:16:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def voice_to_text():
     return transcript['text']
 # もしレコーディングが終わったら
 if audio_bytes:
-    文字起こしした文章をGPTに渡す
+    # 文字起こしした文章をGPTに渡す
     st.session_state["user_input"] = voice_to_text()
     communicate()
 # 文字を入力
