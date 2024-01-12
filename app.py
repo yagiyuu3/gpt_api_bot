3106d1c823ef6cbdc9bd7bd9d14f188ad8101f5f
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2024-01-12 10:55:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -86,11 +86,10 @@ if st.session_state["messages"]:
     messages = st.session_state["messages"]
         
     for i, message in enumerate(reversed(messages[1:])):  # 直近のメッセージを上に
-        speaker = "あなた:"
-        st.write(message)
+        speaker = "あなた"
         if message.role == "assistant":
             speaker = ""
         if 1 >= i >= 0:
-            st.write(speaker + message.content)
+            st.write(speaker + ":" + message.content)
         else:
-            st.caption(speaker + message.content)
+            st.caption(speaker + ":" + message.content)
